daf2c216d3fec42a127375980db5626a8e0e3976
websmartdream <webdream1108@gmail.com>
2022-03-25 15:41:13 +0800

    feat: complete the  user api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -24,9 +24,39 @@ const adminLogin = async (req, res) => {
   res.json({ type: "success", message: "successfull", token });
 };
 
-const userRegister = async (req, res) => {};
+const userRegister = async (req, res) => {
+  const { email } = req.body;
+  const password = await bcrypt.hash(req.body.password, 8);
+  const result = await AuthModel.userRegister({ email, password });
+
+  if (!result.state) {
+    throw new HttpException(400, "This email already exist.");
+  }
+  res.json({ type: "success", message: "successfull" });
+};
+
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const result = await AuthModel.userLogin({ email });
+  if (!result.state) {
+    throw new HttpException(400, "Unregistered User.");
+  }
+  const isMatch = await bcrypt.compare(password, result.user.password);
+  if (!isMatch) {
+    throw new HttpException(400, "Incorrect Password.");
+  }
+  const token = jwt.sign(
+    { user_id: result.user.id.toString() },
+    process.env.SECRET_JWT,
+    {
+      expiresIn: "365d",
+    }
+  );
+  res.json({ type: "success", message: "successfull", token });
+};
 
 module.exports = {
   adminLogin,
   userRegister,
+  userLogin,
 };
--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -19,4 +19,53 @@ const checkAdmin = async (params) => {
   };
 };
 
-module.exports = { checkAdmin };
+const userRegister = async (params) => {
+  const { email, password } = params;
+  const checkSql = `SELECT * FROM ${Tables.tb_users} WHERE email=?`;
+  const result = await DBConnection.query(checkSql, [email]);
+  if (result.length === 0) {
+    const addSql = `INSERT INTO ${Tables.tb_users} (name, firstname, username, password, email, google_account, facebook_account, phone, cities_id, lvl) VALUES (?,?,?,?,?,?,?,?,?,?, now(),now(),now())`;
+    try {
+      await DBConnection.query(addSql, [
+        "Empty",
+        "Empty",
+        "Empty",
+        password,
+        email,
+        "Empty",
+        "Empty",
+        -1,
+        -1,
+      ]);
+      return {
+        state: true,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        state: false,
+      };
+    }
+  }
+  return {
+    state: true,
+  };
+};
+
+const userLogin = async (params) => {
+  const { email } = params;
+  const sql = `SELECT * FROM ${Tables.tb_users} WHERE email=?`;
+  const result = await DBConnection.query(sql, [email]);
+  if (result.length === 0) {
+    return {
+      state: false,
+      user: [],
+    };
+  }
+  return {
+    state: true,
+    user: result[0],
+  };
+};
+
+module.exports = { checkAdmin, userRegister, userLogin };
